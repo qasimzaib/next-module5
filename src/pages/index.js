8ecda1979702ba81b0d4fec4517112df369076d0
qasimzaib <zaib_qasim@hotmail.com>
2023-01-31 13:11:58 +0500

    Dynamic getServerSideProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function HomePage(props) {
 		<>
 			{products.map((product) => (
 				<li key={product.id}>
-					<Link href={`/${product.id}`}>{product.title}</Link>
+					<Link href={`/products/${product.id}`}>{product.title}</Link>
 				</li>
 			))}
 		</>
