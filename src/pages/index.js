af50af9562380189b949a2c2138b4f5865260375
qasimzaib <zaib_qasim@hotmail.com>
2023-01-31 11:51:10 +0500

    getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
-export default function Home() {
+import fs from "fs/promises";
+import path from "path";
+
+export default function HomePage(props) {
+	const { products } = props;
+
 	return (
 		<>
-			<h1>Hello</h1>
+			{products.map((product) => (
+				<li key={product.id}>{product.title}</li>
+			))}
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
+	return {
+		props: {
+			products: data.products,
+		},
+	};
 }
