ce9b83b941d3f61c4fbab2f7836524a40b2ab7e3
qasimzaib <zaib_qasim@hotmail.com>
2023-01-31 12:40:35 +0500

    Dynamic paths

--- src/pages/[pid]/index.js
+++ src/pages/[pid]/index.js
@@ -5,6 +5,10 @@ import path from "path";
 export default function ProductDetailPage(props) {
 	const { product } = props;
 
+	if (!product) {
+		return <p>...Loading</p>;
+	}
+
 	return (
 		<Fragment>
 			<h1>{product.title}</h1>
@@ -13,13 +17,19 @@ export default function ProductDetailPage(props) {
 	);
 }
 
+async function getData() {
+	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
+	return data;
+}
+
 export async function getStaticProps(context) {
 	const { params } = context;
 	const productId = params.pid;
 
-	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-	const jsonData = await fs.readFile(filePath);
-	const data = JSON.parse(jsonData);
+	const data = await getData();
 
 	const product = data.products.find((product) => product.id === productId);
 	return {
@@ -30,12 +40,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+	const data = await getData();
+
+	const ids = data.products.map(product => product.id);
+	const params = ids.map(id => ({ params: { pid: id } }));
+	
 	return {
-		paths: [
-			{ params: { pid: "p1" } },
-			{ params: { pid: "p2" } },
-			{ params: { pid: "p3" } },
-		],
-		fallback: false,
+		paths: params,
+		fallback: true,
 	};
 }
